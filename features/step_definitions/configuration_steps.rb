b93acd149ab7c836f563ba9674d4b255ca66f2f7
dostapenko <dostapenko@hedgeye.com>
2013-06-05 11:17:09 -0300

    delete config

--- features/step_definitions/configuration_steps.rb
+++ features/step_definitions/configuration_steps.rb
@@ -1,28 +1,2 @@
-Then(/^my computer receives it$/) do
-  pending # express the regexp above with the code you wish you had
-end
 
-Then(/^it acknowledges\s+(that |)it received it$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^my computer does not receive it$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^(the|a) (.+?) sends an IPN to (my|the|an|)\s+(\w+)$/) do |dummy, source, dummy2, destination|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the server notifies the developers about the unknown PayPal sandbox$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^(the server|it) sends (an|the) IPN repeatedly for (\d+)( more|) days.+$/) do |dummy, dummy2, dummy3, days|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^(the server|it) (notifies|has notified)\s+(.+?)(\d+) days$/) do |dummy, dummy2, preamble,days|
-  pending # express the regexp above with the code you wish you had
-end
 
