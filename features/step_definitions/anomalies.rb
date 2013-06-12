7d795f1ffc6d711c4e4b883553b67d338e2b6d9c
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDIOQ6x38HqDKXeQtJzDgQSOwhyObc/vqGg0fn/OnvgKxdYhQ2vZAVsJxJyToU3+paFWsa/q6T8sSJlDsMPwhF5T7L1bM78XfU08KZcVe8Sre/1UZzhL+0XW92tgU8yZe0pF0C7R2M9Tp3B38p6dl5sxQ67MY9G8DdXVnrdWZML+lUSgJoArgrLmkfeOh0Pe3Gb4UCATK6/JqzHnANBJWZM8L2gHXhdZl3al4emjP6RkTD4UPtHOozEWWONSdD/ZuxmLcEYEITwC8qhWDEm/CcVIs711CYBnnOk4fjhDNhDgF4lk33F/oDeNgxJev0TtQuSXinfxaVE78vAhnlNJdb dostapenko@hedgeye.comDimitri Ostapenko <dostapenko@hedgeye.com>
2013-06-12 09:40:43 -0400

    all anomalies steps combined into one file

---
+++ features/step_definitions/anomalies.rb
@@ -0,0 +1,96 @@
+Given(/^my computer (is|is not) in PayPal IPN testing mode$/) do |mode|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a test (has|has not) started$/) do |started|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the computer has not polled after (\d+) (hour|days)$/) do |time, time_unit|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the computer's developer is notified that his computer has not polled for (\d+) hour$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the server turns off my computer's test mode$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it notifies me that, after (\d+) days of no polling, my test mode has been turned off$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the server receives a poll request from my computer.*?$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^should notify me that my computer is polling when it shouldn't$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it waits (\d+) hours before sending a repeat notification if needed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^an IPN generating test has started$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the server has not received an IPN from the sandbox for \? minutes$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the server notifies me that it is not receiving any IPNs from my sandbox$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^my computer (is|is not) in test mode$/) do |state|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the server is not launched$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I go into test mode$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^my computer.*?the server that (I'm in test mode|the computer is offline)$/) do |state|
+  #the two versions where: I'm in test mode, and it wants to process IPNs which are the same in my opinion. Comment if you disagree
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the server doesn't respond$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my computer alerts me that the server didn't respond$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the server knows that my computer is in test mode$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^an actual IPN generating test sequence has started on my computer$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^polling has not retrieved any IPNs for this test for (\d+) minutes$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my computer notifies me that it is not receiving any IPNs from the server$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the (server|computer) is in test mode.*?$/) do |subject|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my computer notifies me that the server is not responding to polling$/) do
+  pending # express the regexp above with the code you wish you had
+end
