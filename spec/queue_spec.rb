854b85d51cb585be84bb06001a197e6917c4064a
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDIOQ6x38HqDKXeQtJzDgQSOwhyObc/vqGg0fn/OnvgKxdYhQ2vZAVsJxJyToU3+paFWsa/q6T8sSJlDsMPwhF5T7L1bM78XfU08KZcVe8Sre/1UZzhL+0XW92tgU8yZe0pF0C7R2M9Tp3B38p6dl5sxQ67MY9G8DdXVnrdWZML+lUSgJoArgrLmkfeOh0Pe3Gb4UCATK6/JqzHnANBJWZM8L2gHXhdZl3al4emjP6RkTD4UPtHOozEWWONSdD/ZuxmLcEYEITwC8qhWDEm/CcVIs711CYBnnOk4fjhDNhDgF4lk33F/oDeNgxJev0TtQuSXinfxaVE78vAhnlNJdb dostapenko@hedgeye.comDimitri Ostapenko <dostapenko@hedgeye.com>
2013-06-12 16:29:50 -0400

    rspec tests for queue created

---
+++ spec/queue_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'spec_helper'
+
+describe queue do
+  it 'stores IPNs sent from a sandbox when a computer is testing' do
+    server = Server.new
+  end
+
+  it 'pops IPNs when SendGrid asks for them'
+
+  it 'doesnt store IPNs sent from a sandbox when a computer is not testing'
+  #not sure if this is needed but added just in case. Discuss with Scott
+
+end
