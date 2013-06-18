8be6742fd6f707d42a51c4868fc23644cb7cb430
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDIOQ6x38HqDKXeQtJzDgQSOwhyObc/vqGg0fn/OnvgKxdYhQ2vZAVsJxJyToU3+paFWsa/q6T8sSJlDsMPwhF5T7L1bM78XfU08KZcVe8Sre/1UZzhL+0XW92tgU8yZe0pF0C7R2M9Tp3B38p6dl5sxQ67MY9G8DdXVnrdWZML+lUSgJoArgrLmkfeOh0Pe3Gb4UCATK6/JqzHnANBJWZM8L2gHXhdZl3al4emjP6RkTD4UPtHOozEWWONSdD/ZuxmLcEYEITwC8qhWDEm/CcVIs711CYBnnOk4fjhDNhDgF4lk33F/oDeNgxJev0TtQuSXinfxaVE78vAhnlNJdb dostapenko@hedgeye.comDimitri Ostapenko <dostapenko@hedgeye.com>
2013-06-18 12:34:31 -0400

    rspec tests created

--- lib/send_grid_mail_creater.rb
+++ lib/send_grid_mail_creater.rb
@@ -1,13 +1,22 @@
 class Send_Grid_Mail_Creator
 
   def create(mail)
-    email = Hash.new
-    config = YAML::load_file(File.join(File.dirname(File.expand_path(__FILE__)), 'config.yml'))
-    email = config.clone
-    mail.each_key do |key|
-      email[key] = mail[key]
+    @mail = mail
+    @email = Hash.new
+    load_yml
+    @email = @config.clone
+    combine_params
+    @email
+  end
+
+  def load_yml
+    @config = YAML::load_file(File.join(File.dirname(File.expand_path(__FILE__)), 'config.yml'))
+  end
+
+  def combine_params
+    @mail.each_key do |key|
+      @email[key] = @mail[key]
     end
-    email
   end
 
 end
---
+++ spec/mail_sender.rb
@@ -0,0 +1,10 @@
+describe mail_sender do
+
+  it 'uses a gateway to create the email' do
+  end
+
+  it 'should load a yml file with all the private options for sending an email'
+
+  it 'should put together the fed in paramaters and the private paramters for sending an email'
+
+end
