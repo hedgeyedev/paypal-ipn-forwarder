c34e1d76e2048b5e8fb9081198946db9a3e6210e
Scott Smith <scottnelsonsmith@gmail.com>
2013-06-26 09:20:18 -0700

    Merged lost changes from my development branch

--- lib/router.rb
+++ lib/router.rb
@@ -1,71 +1,52 @@
-require 'rest_client'
-require 'socket'
+require_relative 'poller'
 class Router
 
-  def initialize(target=nil)
-    @target = target
-    test_mode_on
-    @killing = false
-  end
+  TEST_ON = 'on'
+  TEST_OFF = 'off'
 
-  def destroy
-    test_mode_off
-  end
-
-  def poll_for_ipn
-    loop do
-      break if @killing
-      ipn = retrieve_ipn
-      forward_ipn ipn unless(ipn.nil?)
-      sleep 5.0
-    end
-  end
-
-  def kill_poll
-    @killing = true
-  end
-
-  def retrieve_ipn
-     #url = 'http://superbox.hedgeye.com:8810/ipn-response'
-     #url = 'localhost:8810/ipn-response'
-     #ipn = RestClient.get url
-     #ipn
+  def initialize(target)
+    @target  = target
+    @server_url = load_server_url
   end
 
   def forward_ipn(ipn)
-    if(ipn == "VERIFIED")
-       send_verified
+    if (ipn == 'VERIFIED')
+      send_verified
     else
       send_ipn(ipn)
     end
   end
 
-  def send_verified#same functionality as send_verification
+  def send_verified #same functionality as send_verification
     @target.verified
   end
 
   def send_ipn(ipn)
-   @responce = @target.send_ipn(ipn)
+    @target.send_ipn(ipn)
   end
 
   def test_mode_on
-    url = 'http://superbox.hedgeye.com:8810/ipn-response'
-    computer_id = ip_address
-    message = computer_id
-    #RestClient.post url,
-    poll_for_ipn
+    set_test_mode(TEST_ON)
   end
 
   def test_mode_off
-    url = 'http://superbox.hedgeye.com:8810/ipn-response'
-    computer_id = ip_address
-    message = computer_id
-    #RestClient.post url, message
-    kill_poll
+    set_test_mode(TEST_OFF)
   end
 
-  def ip_address #from: http://claudiofloreani.blogspot.com/2011/10/ruby-how-to-get-my-private-and-public.html
-    Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address
+  #from: http://claudiofloreani.blogspot.com/2011/10/ruby-how-to-get-my-private-and-public.html
+  def my_ip_address
+    Socket.ip_address_list.detect { |intf| intf.ipv4_private? }.ip_address
+  end
+
+  private
+
+  def load_server_url
+    url = YAML::load_file(File.expand_path('../../config/config.yml', __FILE__))
+  end
+
+  def set_test_mode(mode)
+    RestClient.post(@server_url, { params: { my_ip: my_ip_address, test_mode: mode
+    } })
   end
 
 end
--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -1,77 +1,59 @@
 require 'rspec'
 require_relative '../lib/router'
+require_relative '../lib/poller'
 require_relative '../lib/server'
 
 describe Router do
 
-  context 'when created' do
-
-    it 'tells the server that test mode has started' do
-      server = mock('Server')
-      server.should_receive(:computer_online).with('developer_one')
-      router = Router.new
-      router.test_mode_on
-    end
-
-    it 'starts polling the server' do
-      router = Router.new
-      router.retrieve_ipn.should == "IPN"
-      #not sure how to test the actuall poll aka the http request
-    end
+  before(:each) do
+    @target     = mock('target')
+    @router     = Router.new(@target)
+    @server_url = YAML::load_file(File.expand_path('../../config/config.yml', __FILE__))
+    @poll       = Poller.new(@router, @server_url)
   end
 
-  context 'exists' do
+  context 'interactions with server' do
 
-    before(:each) do
-      @target = mock('target')
-      @router = Router.new(@target)
-    end
+    context 'test mode' do
 
-    context 'when destroying' do
-
-      it 'stops polling the server' do
-        router = Router.new
-        router.test_mode_on
-        router.test_mode_off
-        defined?(router).should be false
+      def expected_rest_client_message(mode)
+        RestClient.should_receive(:post).with(@server_url, { params: { my_ip:     @router.my_ip_address,
+                                                                       test_mode: mode
+        } })
       end
 
-      it 'tells the server that test mode has finished' do
-        #needs to be changed based on changes on line 9-14 which were made with Scott's help
-        server = Server.new
-        router = Router.new
-        router.test_mode_off
-        server.computer_offline('developer_one')
-        server.computer_online?('developer_one').should == false
+      it 'has started' do
+        expected_rest_client_message(Router::TEST_ON)
+        @router.test_mode_on
       end
 
-      it 'self-destructs'
+      # FIXME or get rid of me
+      #it 'stops polling the server' do
+      #  @router.test_mode_on
+      #  @router.test_mode_off
+      #end
+
+      it 'has stopped' do
+        expected_rest_client_message(Router::TEST_OFF)
+        @router.test_mode_off
+      end
 
     end
 
     context 'polling retrieves an IPN' do
 
-      it 'retrieves an IPN when the server has one to return'
-
-      it 'initiates a protocol to send the IPN to cms'
-      #unsure of what url the http protocol will use to interact with cms
-
-      it 'polls the server again 5 seconds after finishing the protocol with cms'
-      #unsure how to test
+      it 'initiates a protocol to send the IPN to the development computer' do
+        RestClient.should_receive(:post).with(@server_url, @router.my_ip_address)
+      end
 
     end
 
-    context 'polling does not retrieve an IPN' do
+  end
 
-      it 'polls again 5 seconds later'
-      #unsure how to test
+  context 'handshake between router and development computer' do
 
-    end
-
-    context 'handshake between router and CMS' do
-
-      def create_an_ipn_somehow
-        sample_ipn = <<EOF
+    def create_an_ipn_somehow
+      sample_ipn = <<EOF
 mc_gross=19.95&protection_eligibility=Eligible&address_status=confirmed&pay\
 er_id=LPLWNMTBWMFAY&tax=0.00&address_street=1+Main+St&payment_date=20%3A12%\
 3A59+Jan+13%2C+2009+PST&payment_status=Completed&charset=windows-\
@@ -86,10 +68,10 @@ OfCXbDm2hu0ZELryHFjY-Vb7PAUvS6nMXgysbElEn9v-\
 e_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_g\
 ross=19.95&shipping=0.00
 EOF
-      end
+    end
 
-      def create_ipn_response_somehow
-        verified_ipn = <<EOF
+    def create_ipn_response_somehow
+      verified_ipn = <<EOF
 cmd=_notify-validate&mc_gross=19.95&protection_eligibility=Eligible&address_status=confirmed&pay\
 er_id=LPLWNMTBWMFAY&tax=0.00&address_street=1+Main+St&payment_date=20%3A12%\
 3A59+Jan+13%2C+2009+PST&payment_status=Completed&charset=windows-\
@@ -104,30 +86,27 @@ OfCXbDm2hu0ZELryHFjY-Vb7PAUvS6nMXgysbElEn9v-\
 e_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_g\
 ross=19.95&shipping=0.00
 EOF
-      end
+    end
 
+    it 'automatically identifies the developer computer' do
+      @router.my_ip_address.should =~ /\d+\.\d+\.\d+\.\d+/
+    end
 
+    # TODO: development computer and @router are going to have to be tied together
+    it 'processes an IPN' do
+      ipn          = create_an_ipn_somehow
+      ipn_response = create_ipn_response_somehow
+      @target.stub!(:send_ipn).with(ipn).and_return(ipn_response)
+      @router.forward_ipn(ipn)
 
-      # TODO: @cms and @router are going to have to be tied together
-      it 'processes an IPN' do
-        ipn          = create_an_ipn_somehow
-        ipn_response = create_ipn_response_somehow
-        @target.stub!(:send_ipn).with(ipn).and_return(ipn_response)
-        @router.send_ipn(ipn)
+    end
 
-      end
-
-      it 'polls the server for a verfication message'
-
-
-      it 'send a verification message' do
-        @target.should_receive(:verified)
-        server = Server.new
-        server.store_ipn_response('developer_one')
-        server.send_response_to_computer('developer_one').should == "VERIFIED"
-        @router.forward_ipn(server.send_response_to_computer('developer_one'))
-      end
-
+    it 'send a verification message' do
+      @target.should_receive(:verified)
+      server = Server.new
+      server.store_ipn_response('developer_one')
+      server.send_response_to_computer('developer_one').should == 'VERIFIED'
+      @router.forward_ipn(server.send_response_to_computer('developer_one'))
     end
 
   end
