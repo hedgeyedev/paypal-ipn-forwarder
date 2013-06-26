5fc8a4ab0739472ddb5b1373ef00b6729bec1663
Scott Smith <scottnelsonsmith@gmail.com>
2013-06-25 20:06:52 -0700

    Reorganizing context statements to divide the spec file into 2:
    1.  Tests with the server
    2.  Tests with the development computer
    
    Caution: #should_receive is not working (again) with RestClient.

--- lib/router.rb
+++ lib/router.rb
@@ -3,6 +3,7 @@ class Router
 
   def initialize(target)
     @target  = target
+    @server_url = load_server_url
   end
 
   def forward_ipn(ipn)
@@ -21,21 +22,23 @@ class Router
     @target.send_ipn(ipn)
   end
 
-  def load_server_url
-    url = YAML::load_file(File.expand_path('../../config/config.yml', __FILE__))
-  end
-
   def test_mode_on
-    RestClient.post load_server_url, my_ip_address
+    RestClient.post(@server_url, my_ip_address)
   end
 
   def test_mode_off
-    RestClient.post load_server_url, my_ip_address
+    RestClient.post(@server_url, my_ip_address)
   end
 
   #from: http://claudiofloreani.blogspot.com/2011/10/ruby-how-to-get-my-private-and-public.html
   def my_ip_address
     Socket.ip_address_list.detect { |intf| intf.ipv4_private? }.ip_address
+  end
+
+  private
+
+  def load_server_url
+    url = YAML::load_file(File.expand_path('../../config/config.yml', __FILE__))
   end
 
 end
--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -1,4 +1,5 @@
-require 'rspec'
+require 'rest-client'
+require_relative 'spec_helper'
 require_relative '../lib/router'
 require_relative '../lib/poller'
 require_relative '../lib/server'
@@ -6,57 +7,47 @@ require_relative '../lib/server'
 describe Router do
 
   before(:each) do
-    @target = mock('target')
+    @target     = mock('target')
+    @router     = Router.new(@target)
+    @server_url = YAML::load_file(File.expand_path('../../config/config.yml', __FILE__))
+    @poll       = Poller.new(@router, @server_url)
   end
 
-  context 'when created' do
+  context 'interactions with server' do
 
     it 'tells the server that test mode has started' do
-      server_url = YAML::load_file(File.expand_path('../../config/config.yml', __FILE__))
-      router     = Router.new(@target)
-      RestClient.should_receive(:post).with(server_url, router.my_ip_address)
-      router.test_mode_on
+      RestClient.should_receive(:post).with(@server_url, { params: { my_ip:     @router.my_ip_address,
+                                                                     test_mode: 'on'
+      } })
+      @router.test_mode_on
     end
 
-  end
-
-  context 'exists' do
-
-    before(:each) do
-      @router = Router.new(@target)
-      @poll   = Poller.new(@router, 'http://superbox.hedgeye.com:8810/test')
+    # FIXME or get rid of me
+    it 'stops polling the server' do
+      @router.test_mode_on
+      @router.test_mode_off
     end
 
-    context 'when destroying' do
-
-      it 'stops polling the server' do
-        router = Router.new
-        @router.test_mode_on
-        @router.test_mode_off
-        defined?(router).should be false
-      end
-
-      it 'tells the server that test mode has finished' do
-        #needs to be changed based on changes on line 9-14 which were made with Scott's help
-        server = Server.new
-        @router.test_mode_off
-        server.computer_testing('developer_one')
-        server.computer_online?('developer_one').should == false
-      end
-
+    it 'tells the server that test mode has finished' do
+      RestClient.should_receive(:post).with(@server_url, { params: { my_ip:     @router.my_ip_address,
+                                                                     test_mode: 'off'
+      } })
+      @router.test_mode_off
     end
 
     context 'polling retrieves an IPN' do
 
       it 'initiates a protocol to send the IPN to the development computer'
-      #unsure of what url the http protocol will use to interact with development computer
+      RestClient.should_receive(:post).with(@server_url, @router.my_ip_address)
 
     end
 
-    context 'handshake between router and development computer' do
+  end
 
-      def create_an_ipn_somehow
-        sample_ipn = <<EOF
+  context 'handshake between router and development computer' do
+
+    def create_an_ipn_somehow
+      sample_ipn = <<EOF
 mc_gross=19.95&protection_eligibility=Eligible&address_status=confirmed&pay\
 er_id=LPLWNMTBWMFAY&tax=0.00&address_street=1+Main+St&payment_date=20%3A12%\
 3A59+Jan+13%2C+2009+PST&payment_status=Completed&charset=windows-\
@@ -71,10 +62,10 @@ OfCXbDm2hu0ZELryHFjY-Vb7PAUvS6nMXgysbElEn9v-\
 e_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_g\
 ross=19.95&shipping=0.00
 EOF
-      end
+    end
 
-      def create_ipn_response_somehow
-        verified_ipn = <<EOF
+    def create_ipn_response_somehow
+      verified_ipn = <<EOF
 cmd=_notify-validate&mc_gross=19.95&protection_eligibility=Eligible&address_status=confirmed&pay\
 er_id=LPLWNMTBWMFAY&tax=0.00&address_street=1+Main+St&payment_date=20%3A12%\
 3A59+Jan+13%2C+2009+PST&payment_status=Completed&charset=windows-\
@@ -89,29 +80,27 @@ OfCXbDm2hu0ZELryHFjY-Vb7PAUvS6nMXgysbElEn9v-\
 e_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_g\
 ross=19.95&shipping=0.00
 EOF
-      end
+    end
 
-      it 'automatically identifies the developer computer' do
-        @router.my_ip_address.should =~ /\d+\.\d+\.\d+\.\d+/
-      end
+    it 'automatically identifies the developer computer' do
+      @router.my_ip_address.should =~ /\d+\.\d+\.\d+\.\d+/
+    end
 
-      # TODO: development computer and @router are going to have to be tied together
-      it 'processes an IPN' do
-        ipn          = create_an_ipn_somehow
-        ipn_response = create_ipn_response_somehow
-        @target.stub!(:send_ipn).with(ipn).and_return(ipn_response)
-        @router.forward_ipn(ipn)
+    # TODO: development computer and @router are going to have to be tied together
+    it 'processes an IPN' do
+      ipn          = create_an_ipn_somehow
+      ipn_response = create_ipn_response_somehow
+      @target.stub!(:send_ipn).with(ipn).and_return(ipn_response)
+      @router.forward_ipn(ipn)
 
-      end
+    end
 
-      it 'send a verification message' do
-        @target.should_receive(:verified)
-        server = Server.new
-        server.store_ipn_response('developer_one')
-        server.send_response_to_computer('developer_one').should == 'VERIFIED'
-        @router.forward_ipn(server.send_response_to_computer('developer_one'))
-      end
-
+    it 'send a verification message' do
+      @target.should_receive(:verified)
+      server = Server.new
+      server.store_ipn_response('developer_one')
+      server.send_response_to_computer('developer_one').should == 'VERIFIED'
+      @router.forward_ipn(server.send_response_to_computer('developer_one'))
     end
 
   end
