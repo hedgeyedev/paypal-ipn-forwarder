d316fdbc30ca5de27bd99530dcc8539228fa4ed3
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDIOQ6x38HqDKXeQtJzDgQSOwhyObc/vqGg0fn/OnvgKxdYhQ2vZAVsJxJyToU3+paFWsa/q6T8sSJlDsMPwhF5T7L1bM78XfU08KZcVe8Sre/1UZzhL+0XW92tgU8yZe0pF0C7R2M9Tp3B38p6dl5sxQ67MY9G8DdXVnrdWZML+lUSgJoArgrLmkfeOh0Pe3Gb4UCATK6/JqzHnANBJWZM8L2gHXhdZl3al4emjP6RkTD4UPtHOozEWWONSdD/ZuxmLcEYEITwC8qhWDEm/CcVIs711CYBnnOk4fjhDNhDgF4lk33F/oDeNgxJev0TtQuSXinfxaVE78vAhnlNJdb dostapenko@hedgeye.comDimitri Ostapenko <dostapenko@hedgeye.com>
2013-07-22 10:26:24 -0400

    "server client spec written"

--- spec/server_client_spec.rb
+++ spec/server_client_spec.rb
@@ -3,10 +3,16 @@ require_relative '../lib/server_client'
 
 describe ServerClient do
 
-  it 'should turn on testing'
+  it 'should receive a testing ON HTTP request from the router and turn ON testing for the sandbox'
 
-  it 'should turn off testing'
+  it 'should receive a testing OFF HTTP request from the router and turn OFF testing for the sandbox'
 
   it 'should receive a poll from a development computer and respond to it'
+
+  it 'should receive IPNs and forward them to the server'
+
+  it 'should create the response to a sandbox when it has sent an IPN'
+
+  it 'should not respond to VERIFIED messages send from the sandbox'
 
 end
