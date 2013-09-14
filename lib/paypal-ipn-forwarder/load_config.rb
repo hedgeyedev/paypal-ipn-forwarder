951cf27212a05b9fbc0244316d7c5e4f7575b227
Scott Smith <scottnelsonsmith@gmail.com>
2013-09-13 18:10:56 -0700

    [#55756440] Half the regression tests pass

--- lib/paypal-ipn-forwarder/load_config.rb
+++ lib/paypal-ipn-forwarder/load_config.rb
@@ -5,7 +5,7 @@ module PaypalIpnForwarder
 
     def initialize
       dev_version = @@test_mode ? '_test' : ''
-      @config = YAML::load_file(File.expand_path("../../config#{dev_version}.yml", __FILE__))
+      @config = YAML::load_file(File.expand_path("../../../config#{dev_version}.yml", __FILE__))
     end
 
     def self.set_test_mode(mode_boolean)
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -2,11 +2,11 @@ require_relative 'spec_helper'
 require_relative '../lib/paypal-ipn-forwarder/server'
 require_relative '../lib/paypal-ipn-forwarder/ipn_generator'
 
-  describe PaypalIpnForwarder::Server do
+describe PaypalIpnForwarder::Server do
 
   before(:each) do
     @server = PaypalIpnForwarder::Server.new(TEST_MODE_ON)
-    @server.begin_test_mode('my_sandbox_id', {'my_sandbox_id' => 'my_sandbox_id', 'test_mode' => 'on', '@email' => 'bob@example.com'})
+    @server.begin_test_mode('my_sandbox_id', { 'my_sandbox_id' => 'my_sandbox_id', 'test_mode' => 'on', '@email' => 'bob@example.com' })
   end
 
   it 'turns on test mode for a computer once it receives a test-mode message' do
@@ -19,8 +19,9 @@ require_relative '../lib/paypal-ipn-forwarder/ipn_generator'
 
     it 'retrieves the Paypal sandbox id from the IPN' do
       ipn_generator = PaypalIpnForwarder::IpnGenerator.new
-      sample_ipn = ipn_generator.ipn
-      paypal_id = @server.paypal_id(sample_ipn)
+      sample_ipn    = ipn_generator.ipn
+      ap sample_ipn;1
+      paypal_id     = @server.paypal_id(sample_ipn)
       paypal_id.should == 'my_sandbox_id'
     end
 
