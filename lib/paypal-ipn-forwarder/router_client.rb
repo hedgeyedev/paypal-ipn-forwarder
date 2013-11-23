1b6466f83bc88bc3c3f0b028d9ed8050d68b4f2d
Scott Smith <scottnelsonsmith@gmail.com>
2013-11-22 17:56:06 -0800

    renamed no longer-existing variable/method name

--- lib/paypal-ipn-forwarder/router_client.rb
+++ lib/paypal-ipn-forwarder/router_client.rb
@@ -7,13 +7,13 @@ module PaypalIpnForwarder
       @config = LoadConfig.new(set_test_mode)
       @server_url = @config.server_url
       @server_url += 'test'
-      @final_destination_url = @config.final_destination_url
+      @development_computer_url = @config.development_computer_url
     end
 
     def send_ipn(ipn)
       puts 'An IPN was received! I am now trying to send it to the final destination.'
       begin
-        RestClient.post @final_destination_url, ipn
+        RestClient.post @development_computer_url, ipn
       rescue StandardError
         puts 'There was an error with the App where the IPN needs to be delivered. Please make sure it is running'
       end
