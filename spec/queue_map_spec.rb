47e620fa3a0d50828995ececebab8dfe2df878e0
Scott Smith <scottnelsonsmith@gmail.com>
2013-12-16 16:49:23 -0800

    [#55756440] Added separate queue and queue map classes; don't know whether we'll use them

---
+++ spec/queue_map_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require_relative '../lib/paypal-ipn-forwarder/queue_map'
+
+include PaypalIpnForwarder
+
+describe QueueMap do
+
+  it 'should ' do
+    true.should == true
+  end
+end
